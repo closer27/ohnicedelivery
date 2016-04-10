68a7ca567f69aa674aed3846dd1c64b79c65fca0
Seungwon Kim <closer527@gmail.com>
2016-04-10 20:45:18 +0900

    apply writing method, getting file path to save, add requirements.txt

--- run.py
+++ run.py
@@ -1,12 +1,24 @@
 __author__ = 'wonny'
 
 import sys
+import os
+
+
+def get_newfile_path(current_file_path):
+    import time
+    today = time.strftime("플래네틸_%Y%m%d_라투투발주리스트")
+    output_dir = os.path.dirname(current_file_path)
+    return os.path.join(output_dir, today + '.xls')
 
 if __name__ == '__main__':
     if len(sys.argv) > 1:
-        print('file name : ' + sys.argv[1])
+        filepath = sys.argv[1]
+        print('file name : ' + filepath)
         from OrderParser.book import parse
-        parse(sys.argv[1])
+        order_list = parse(filepath)
+
+        from OrderParser.writer import write_to_xls
+        write_to_xls(order_list, get_newfile_path(filepath))
 
     else:
         print('not correct file, please check again')
