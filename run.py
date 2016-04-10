043a78dc49363a094e837ea951a3379a7f7d920a
Seungwon Kim <closer527@gmail.com>
2016-04-10 19:02:38 +0900

    print file name

---
+++ run.py
@@ -0,0 +1,9 @@
+__author__ = 'wonny'
+
+import sys
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        print('file name : ' + sys.argv[1])
+    else:
+        print('not correct file, please check again')
