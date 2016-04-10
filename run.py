4adae84cb09e3f04ded0323a7238191d4e7ec1a8
Seungwon Kim <closer527@gmail.com>
2016-04-10 19:43:02 +0900

    create OrderParser package, create module to read csv file

--- run.py
+++ run.py
@@ -5,5 +5,8 @@ import sys
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         print('file name : ' + sys.argv[1])
+        from OrderParser.book import parse
+        parse(sys.argv[1])
+
     else:
         print('not correct file, please check again')
