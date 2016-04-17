5b4fae20e8947e0e588eac30c195f827023864b7
김승원(Seungwon Kim)/기술전략실/SKP <seungwon@sk.com>
2016-04-18 00:41:29 +0900

    bugfix - get file name from time.strftime() ; because of Korean encoding showing file names added to the label

--- run.py
+++ run.py
@@ -54,43 +54,39 @@ class MyWidget(QWidget):
     def process(self, input_path):
         self.input_path_arr.append(input_path)
 
-        self.label.setText(input_path + "가 추가되었습니다.")
+        path_str = "현재 추가 된 발주리스트(중복해서 넣지마세요!)\n"
+        for path in self.input_path_arr:
+            path_str += path + "\n"
+        self.label.setText(path_str)
 
     def progress_spreadsheet(self):
+        if len(self.input_path_arr) <= 0:
+            return
+
         from OrderParser.reader import get_orderlist
         self.order_data = get_orderlist(self.input_path_arr)
 
         from OrderParser.writer import write_to_xls
-        write_to_xls(self.order_data, self.get_newfile_path(sys.argv[1]))
+        write_to_xls(self.order_data, self.get_newfile_path(self.input_path_arr[0]))
 
         self.progressBar.setRange(0, 0)
+        self.label.setText("변환 완료! 발주서와 같은 폴더에 생겼어요.")
 
     def get_newfile_path(self, current_file_path):
         import time
-        today = time.strftime("플래네틸_%Y%m%d_라투투발주리스트")
+        today = "{}{}{}".format('플래티넘_',time.strftime("%Y%m%d"), '_라투투발주리스트')
         output_dir = os.path.dirname(current_file_path)
+        self.label.setText(today)
         return os.path.join(output_dir, today + '.xls')
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        filepaths = []
-        for argv in sys.argv[1:]:
-            filepaths.append(argv)
 
-        # order_list = get_orderlist(filepaths)
-        #
-        # write_to_xls(order_list, get_newfile_path(sys.argv[1]))
+    app = QApplication(sys.argv)
+    w = MyWidget()
+    w.resize(250, 150)
+    w.move(300, 300)
+    w.setWindowTitle("Delivery Swimmer")
+    w.show()
 
-        app = QApplication(sys.argv)
-
-        w = MyWidget()
-        w.resize(250, 150)
-        w.move(300, 300)
-        w.setWindowTitle("Delivery Swimmer")
-        w.show()
-
-        sys.exit(app.exec_())
-
-else:
-    print('not correct file, please check again')
+    sys.exit(app.exec_())
