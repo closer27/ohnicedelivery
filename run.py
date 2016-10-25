a137e6a31e0c37eaff3ded50b7eecddc9f058a05
Seungwon Kim <closer527@gmail.com>
2016-10-25 19:44:22 +0900

    modify host ip

--- run.py
+++ run.py
@@ -52,5 +52,4 @@ def upload_file():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=int(sys.argv[1]))
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(sys.argv[1]))
