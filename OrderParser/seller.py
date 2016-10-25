6354772d9ea8b7b72c0b333ca73cb07c5b1ac778
Seungwon Kim <closer527@gmail.com>
2016-10-25 17:49:06 +0900

    add Seller class describing sheet indexes and subclasses inheriting Seller

---
+++ OrderParser/seller.py
@@ -0,0 +1,86 @@
+class Seller:
+    idx_recipient_name = 0
+    idx_recipient_phone = 0
+    idx_zipcode = 0
+    idx_address = 0
+    idx_order_num = 0
+    idx_product_name = 0
+    idx_product_option = 0
+    idx_caution = 0
+
+
+class TenByTen(Seller):
+    idx_recipient_name = 6
+    idx_recipient_phone = 8
+    idx_zipcode = 9
+    idx_address = 10
+    idx_order_num = 0
+    idx_product_name = 14
+    idx_product_option = 15
+    idx_caution = 11
+
+
+class Book(Seller):
+    idx_recipient_name = 7
+    idx_recipient_phone = 8
+    idx_zipcode = 10
+    idx_address = 11
+    idx_order_num = 0
+    idx_product_name = 2
+    idx_product_option = 5
+    idx_caution = 12
+
+
+class StoreFarm(Seller):
+    idx_recipient_name = 9
+    idx_recipient_phone = 56
+    idx_zipcode = 41
+    idx_address = 39
+    idx_order_num = 1
+    idx_product_name = 15
+    idx_product_option = 17
+    idx_caution = 42
+
+
+class Uniqmoment(Seller):
+    idx_recipient_name = 2
+    idx_recipient_phone = 10
+    idx_zipcode = 8
+    idx_address = 9
+    idx_order_num = 0
+    idx_product_name = 4
+    idx_product_option = 3
+    idx_caution = 11
+
+
+class Babosarang(Seller):
+    idx_recipient_name = 15
+    idx_recipient_phone = 16
+    idx_zipcode = 18
+    idx_address = 19
+    idx_order_num = 0
+    idx_product_name = 5
+    idx_product_option = 6
+    idx_caution = 21
+
+
+class Biskit(Seller):
+    idx_recipient_name = 9
+    idx_recipient_phone = 14
+    idx_zipcode = 10
+    idx_address = 11
+    idx_order_num = 0
+    idx_product_name = 2
+    idx_product_option = 4
+    idx_caution = 15
+
+
+class Aland(Seller):
+    idx_receiptor_name = 7
+    idx_receiptor_phone = 9
+    idx_zipcode = 10
+    idx_address = 11
+    idx_order_num = 2
+    idx_product_name = 18
+    idx_product_option = 20
+    idx_caution = 12
